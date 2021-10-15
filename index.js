36c55316d553680808cabdf422d0aa1005a0048a
Nilank <sharmanilank@gmail.com>
2021-10-15 19:02:34 +0530

    Add HTTP PATCH  request for Updating  User By Id

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
 const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/backendApp', { 
@@ -26,15 +26,30 @@ app.get("/users", async (req,res) => {
 app.get("/users/:id", async(req,res)=>{
     const user = await User.findById(req.params.id);
     res.status(200).send(user);
-})
+});
 
 app.post("/users", async (req,res) => {
     User.create(req.body).then((result) => {
         res.status(201).send({id:result._id});
     })
-
-
 });
+
+
+app.patch("/users/:id",async(req,res)=>{
+    try{
+        const user = await User.findOne({_id:req.params.id})
+        if(req.body){
+            user = req.body
+        }
+
+        await user.save();
+        res.status(200).send(user);
+    }catch{
+        res.status(404);
+        res.send({error: "User doesn't exist!!"})
+    }
+})
+
 
 app.delete("/users/:id",async(req,res) => {
     User.findByIdAndRemove(req.params.id).then((result)=>{
